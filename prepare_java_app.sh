7dd9094722b9915b60a2cb0413d9b68320c10ea0
BuildTools <git@kelm.net>
2021-05-12 21:41:29 +0000

    fix: check wrong file

--- prepare_java_app.sh
+++ prepare_java_app.sh
@@ -2,7 +2,7 @@
 # Create links for all plugins in server directory.
 mkdir -p "${SERVER_DIR}/mods"
 for filename in ${MODS_JAR_DIR}/*.jar ; do
-    test -e "$filename" || continue
-    echo prepare_java_app: Linking mod jar $filename to ${SERVER_DIR}/mods/$(basename $filename)
+    test -e "${SERVER_DIR}/mods/$(basename $filename)" || continue
+    echo "prepare_java_app: Linking mod jar ${filename} to ${SERVER_DIR}/mods/$(basename $filename)"
     ln -s "${filename}" "${SERVER_DIR}/mods/$(basename $filename)"
 done
